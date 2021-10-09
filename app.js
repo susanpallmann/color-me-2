11beaeb38ea919117f7205fffcad4413da125a22
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-08 23:34:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -19,16 +19,25 @@ $('document').ready(function() {
     });
 });
 
-/*// Sign In
-firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+$('document').ready(function() {
+    $("#log-in-form").submit(function(event){
+        event.preventDefault();
+        let email = $('#log-in-email').val();
+        let password = $('#log-in-password').val();
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                $('#authenticate').find('.error-field p').text(errorMessage);
+                // ..
+        });
+        return false;
+    });
 });
 
 
