c0063eb95d366db4b79b33e015dc9306c5ae1f32
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-08 23:07:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// Test
+// Sign Up
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+// Sign In
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+// Sign Out
+firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
