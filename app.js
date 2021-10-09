63c615cf719a2c0427e94d472bb67df0a3717c03
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-08 23:24:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
 $("#sign-up-form").submit(function(event){
+    let email = $('#sign-up-email').val();
+    let password = $('#sign-up-password').val();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            $('#authenticate').find('.error-field p').text(errorMessage);
+            // ..
+    });
     event.preventDefault();
 });
 
-// Sign Up
-firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Signed in 
-        var user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        $('#authenticate').find('.error-field p').text(errorMessage);
-        // ..
-});
-
-// Sign In
+/*// Sign In
 firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
         // Signed in
@@ -37,3 +37,4 @@ firebase.auth().signOut()
     }).catch((error) => {
         // An error happened.
 });
+*/
