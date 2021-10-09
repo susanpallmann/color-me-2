b8a5ec39062700293cf563c398d1e06b3c1d4050
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-08 23:22:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
+
+$("#sign-up-form").submit(function(event){
+    event.preventDefault();
+});
+
 // Sign Up
 firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    $('#authenticate').find('.error-field p').text(errorMessage);
-    // ..
-  });
+    .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        $('#authenticate').find('.error-field p').text(errorMessage);
+        // ..
+});
 
 // Sign In
 firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+    .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+});
+
 
 // Sign Out
-firebase.auth().signOut().then(() => {
-  // Sign-out successful.
-}).catch((error) => {
-  // An error happened.
+firebase.auth().signOut()
+    .then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
 });
