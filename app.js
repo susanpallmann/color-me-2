7bc1536ccbc321d00ee9dffba94026f371bf2469
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-11 21:56:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -213,6 +213,13 @@ $('document').ready(function() {
                         let mood = data.mood;
                         let hue = data.hue;
                         let notes = data.notes;
+                        let formattedDate = date;
+                        formattedDate = formattedDate.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
+                        $('post-date').text()
+                        $('input[name="post-day-mood"]').prop('checked', false);
+                        $(`post-day-mood-${mood}`).prop('checked'), true);
+                        $('#post-day-hue').val(hue);
+                        $('#notes').val(notes);
                     } else {
                         // No data found
                     }
