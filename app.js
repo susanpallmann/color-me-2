0a0733c29ab4159db55347e54390ef7176be3ee6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-10-11 20:02:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -126,13 +126,12 @@ $('document').ready(function() {
             hue: hue,
             notes, notes
         };
-        console.log(data);
         let today = new Date();
         let dd = String(today.getDate()). padStart(2, '0');
         let mm = String(today.getMonth() + 1). padStart(2, '0'); //January is 0!
         let yyyy = today.getFullYear();
         let date = yyyy + mm + dd;
-        console.log(date);
+        logDay(date, data);
         return false;
     });
 });
